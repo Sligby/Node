08117d0b641b10e87e65ebe1f9a3817e04f15891
root <root@LAPTOP-N1AEFKV0>
2023-08-30 14:32:00 -0400

    error handling properly

--- shoplist/itemRoutes.js
+++ shoplist/itemRoutes.js
@@ -5,64 +5,96 @@ const items = require('./fakeDb')
 
 
 router.get('/', (req, res) =>{
-    res.json({items : items})
-}
-)
+    try{
+        if(!items){
+            throw new ExpressError('List Not Found', 404)
+        }
+        res.json({items : items})
+    }
+    catch(err){
+        next(err)
+    }
+})
 
 router.post('/', (req, res) => {
     const newItem = req.body;
-    items.push(newItem);
+    try{ 
+        if(!newItem){
+            throw new ExpressError('No Item Submitted!', 404)
+        }
+        else{
+            items.push(newItem);
   
-    res.status(201).json({
-      added: newItem
-    });
-  });
+            res.status(201).json({
+                added: newItem
+            });
+        }
+    }
+    catch(err){
+        next(err)
+    }
+});
 
 router.get('/:name', (req, res)=>{
    const itemName = req.params.name;
    const item = list.find(item=> item.name === itemName);
 
-   if(item){
-        res.json({
-            name: item.name,
-            price: item.price
-        });
-    
+    try{
+        if(!item){
+            throw new ExpressError('Item Not Found', 404)
+
+        }
+        else{
+            return res.json({
+                    name: item.name,
+                    price: item.price
+                   });
+           
+        };
     }
-    else{
-        throw new ExpressError('Item Not Found', 404) 
-    };
+    catch(err){
+        return next(err)
+    }
 });
 
 router.patch('/:name', (req, res) => {
     const itemName = req.params.name;
     const item = list.find(item => item.name === itemName);
 
-    if(item){
-        item.name = req.body.name
-        item.price = req.body.price
+    try{
+        if(!item){
+            throw new ExpressError('Item Not Found', 404)
+        }
+        else{
+            item.name = req.body.name
+            item.price = req.body.price
+        };
+        return res.json({ 
+            updated :{name : req.body.name, price : req.body.price}
+               })
     }
-    else{
-        throw new ExpressError('Item Not Found', 404)
-    };
-    res.json({ 
-        updated :{name : req.body.name, price : req.body.price}
-    })
+    catch(err){
+        return next(err)
+    }
 });
 
 router.delete('/:name', (req, res) => {
    const itemName = req.params.name;
    const item = list.find(item => item.name === itemName);
-   
-   if(item){
-    items.splice(item, 1)
-    res.json({
-        message: "DELETED SUCCESFULLY"
-    })
-   }
-   else{
-    throw new ExpressError('Item Not Found', 404)
-   };
+    try{
+        if(item){
+            items.splice(item, 1)
+            res.json({
+                message: "DELETED SUCCESFULLY"
+            })
+        }
+        else{
+            throw new ExpressError('Item Not Found', 404)
+        };
+    }
+    catch(err){
+        return next(err)
+    }
 });
 
 module.exports = router;
